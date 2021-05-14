8dcd5f13316707b5167969c6df75a07225198bfd
Inder Barthwal <42334718+inderbarthwal@users.noreply.github.com>
2021-05-14 12:41:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def uploads():
         f.save(file_path)
         test1()
 
-        return render_template("base.html")
+        return render_template("base.html", name=f.filename)
     return None
 
     
